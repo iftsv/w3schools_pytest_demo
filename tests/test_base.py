cca1b93e51a20c517dafa2ef12203f1b0c72b2e2
ilya.fetisov <27852432+iftsv@users.noreply.github.com>
2023-08-18 12:00:11 +0700

    fix: chrome running

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,9 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.edge.service import Service as EdgeService
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from selenium.webdriver.edge.options import Options as EdgeOptions
 
 
 @pytest.mark.usefixtures("init_driver")
@@ -13,7 +10,7 @@ class BaseTest:
 
     web_driver = None
 
-    @pytest.fixture(params=["chrome", "edge"], scope='class')
+    @pytest.fixture(params=["chrome"], scope='class')
     def init_driver(self, request):
 
         if request.param == "chrome":
@@ -25,15 +22,6 @@ class BaseTest:
             self.web_driver = webdriver.Chrome(
                 service=ChromeService(ChromeDriverManager().install()),
                 options=chrome_options
-            )
-
-        if request.param == "edge":
-            edge_options = EdgeOptions()
-            edge_options.add_argument("--headless")
-            edge_options.add_argument("--log-level=3")
-            self.web_driver = webdriver.Edge(
-                service=EdgeService(EdgeChromiumDriverManager().install()),
-                options=edge_options
             )
 
         request.cls.driver = self.web_driver
